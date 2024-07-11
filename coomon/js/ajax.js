77e280931f3370125e9d9cde9a2dc1ae31bb233d
code60c <120358990+code60c@users.noreply.github.com>
2024-07-11 11:54:39 +0530

    Update ajax.js

--- coomon/js/ajax.js
+++ coomon/js/ajax.js
@@ -1,1 +1,42 @@
+// Common AJAX function with jQuery
+function ajaxRequest({
+    url,
+    method = 'GET',
+    headers = {},
+    data = null,
+    dataType = 'json',
+    contentType = 'application/json',
+    onSuccess = (response) => console.log(response),
+    onError = (error) => console.error(error),
+    onFinally = () => {}
+}) {
+    $.ajax({
+        url: url,
+        type: method,
+        headers: headers,
+        data: data ? JSON.stringify(data) : null,
+        dataType: dataType,
+        contentType: contentType,
+        success: function(response) {
+            onSuccess(response);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            onError({ jqXHR, textStatus, errorThrown });
+        },
+        complete: function() {
+            onFinally();
+        }
+    });
+}
 
+// POST request with data and alert message on success
+ajaxRequest({
+    url: '/api/submit-data',
+    method: 'POST',
+    data: { key1: 'value1', key2: 'value2' },
+    onSuccess: (response) => {
+        console.log('Data submitted successfully:', response);
+        alert('Data submitted successfully!');
+    },
+    onError: (error) => console.error('Error occurred:', error)
+});
